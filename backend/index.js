e86623a97b1e2ec85a5c668e0ca3d892747605a4
Vinayak Kamble <110101863+vinayakmk19@users.noreply.github.com>
2024-07-28 13:01:43 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -156,10 +156,10 @@ app.use((err, req, res, next) => {
 });
 
 // PRODUCTION
-app.use(express.static(path.join(__dirname, "../frontend/dist")));
+app.use(express.static(path.join(__dirname, "../frontend")));
 
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "../frontend/dist", "index.html"));
+  res.sendFile(path.join(__dirname, "../frontend", "index.html"));
 });
 
 
