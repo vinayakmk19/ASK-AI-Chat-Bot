85f19b2faeb7aaabb8b831eadb6f7b59eb6bcae3
vinayakmk19 <vinayakmkgen@gmail.com>
2024-07-29 09:34:15 +0530

    testing

--- backend/index.js
+++ backend/index.js
@@ -14,6 +14,8 @@ const app = express();
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
+console.log(process.env.CLIENT_URL);
+
 app.use(
   cors({
     origin: process.env.CLIENT_URL,
